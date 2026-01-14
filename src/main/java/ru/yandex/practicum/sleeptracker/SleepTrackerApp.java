2e4bc05fdcf0240e9f528d5297d9b51a2d988a49
Ilya Makarov <Ilya15040@yandex.ru>
2026-01-14 23:30:32 +0400

    Реализован каркас приложения, и чтение файла с конвертацией в сессию сна

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
@@ -1,8 +1,32 @@
 package ru.yandex.practicum.sleeptracker;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SleepTrackerApp {
-
     public static void main(String[] args) {
+        List<SleepingSession> sleepingSessions = new ArrayList<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm");
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(args[0]))){
+            bufferedReader.lines()
+                    .map(s -> s.split(";"))
+                    .filter(strings -> strings.length == 3)
+                    .forEach(strings -> sleepingSessions.add(
+                            new SleepingSession(
+                                    LocalDateTime.parse(strings[0], formatter),
+                                    LocalDateTime.parse(strings[1], formatter),
+                                    SleepAssessment.valueOf(strings[2]))));
 
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
